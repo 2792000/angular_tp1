dba08085a625b92afc59756947363e0544ca3118
AMINE <57amine6@gmail.com>
2024-04-18 21:46:24 +0100

    angular_tp1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  number =0;
+  addNumber(){
+    this.number=this.number+1
+  }
+
+}
